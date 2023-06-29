1c28e0b7503a4825f6672645d631dda021c8c252
Emin Tatar <emintatar48@outlook.com>
2023-06-29 16:44:37 +0300

    Updated

---
+++ Logic-1/love6/solution.java
@@ -0,0 +1,11 @@
+public boolean love6(int a, int b) 
+{
+  int sum = a + b;
+  int diff = Math.abs(a - b);
+  if (a == 6 || b == 6 || sum == 6 || diff == 6)
+  {
+    return true;
+  }
+  
+  return false;
+}
