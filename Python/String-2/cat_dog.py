0d2c3dd42e8fc46d973d0997beb367b90bf476a2
Abdullah Emin Tatar <emintatar00@gmail.com>
2023-09-22 18:29:43 +0300

    Update

---
+++ Python/String-2/cat_dog.py
@@ -0,0 +1,19 @@
+"""
+Return True if the string "cat" and "dog" appear the same number of times in the given string.
+
+
+cat_dog('catdog') → True
+cat_dog('catcat') → False
+cat_dog('1cat1cadodog') → True
+"""
+
+
+def cat_dog(str):
+    cat_count = 0
+    dog_count = 0
+    for i in range(len(str) - 2):
+        if (str[i] == "c" and str[i + 1] == "a" and str[i + 2] == "t"):
+            cat_count += 1
+        if (str[i] == "d" and str[i + 1] == "o" and str[i + 2] == "g"):
+            dog_count += 1
+    return cat_count == dog_count
---
+++ Python/String-2/xyz_there.py
@@ -0,0 +1,15 @@
+"""
+Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+
+
+xyz_there('abcxyz') → True
+xyz_there('abc.xyz') → False
+xyz_there('xyz.abc') → True
+"""
+
+
+def xyz_there(str):
+    for i in range(len(str) - 2):
+        if (str[i:i+3] == "xyz" and (str[i - 1] != "." or i == 0)):
+            return True
+    return False
